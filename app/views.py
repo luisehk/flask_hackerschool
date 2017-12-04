4e7ce69aa3ecba0cb4d7ecc4973d9d59af52ad69
Luis Herrada <luisehk@gmail.com>
2017-12-04 00:41:47 -0600

    Intro to templating

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello, world"
+    user = {'nickname': 'Luis'}
+    return render_template(
+        'index.html',
+        title='Tu nueva aplicación',
+        user=user
+    )
